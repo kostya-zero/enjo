eccb6c4522a4153fcd802d7d93c42976815fc35c
Konstantin Zhigaylo <zero@kostyazero.com>
2024-08-17 14:31:26 +0300

    refactor(test): rewrite tests for library

--- src/tests/test_library.rs
+++ src/tests/test_library.rs
@@ -1,122 +1,49 @@
-use crate::errors::LibraryError;
-use crate::library::{Library, Project};
+use crate::library::Library;
 use std::fs;
-use std::path::PathBuf;
 use tempfile::tempdir;
 
 #[test]
-fn test_project_new() {
-    let project = Project::new("test_project", PathBuf::from("/test/path"));
-    assert_eq!(project.get_name(), "test_project");
-    assert_eq!(project.get_path_str(), "/test/path");
-}
-
-#[test]
-fn test_project_get_path_str() {
-    let project = Project::new("test_project", PathBuf::from("/test/path"));
-    assert_eq!(project.get_path_str(), "/test/path");
-}
-
-#[test]
-fn test_container_new_directory_not_found() {
-    let result = Library::new("/nonexistent/path", false);
-    assert!(matches!(result, Err(LibraryError::DirectoryNotFound)));
-}
-
-#[test]
-fn test_container_new_read_failed() {
+fn test_library_new() {
     let temp_dir = tempdir().unwrap();
-    let file_path = temp_dir.path().join("not_a_directory");
-    fs::File::create(&file_path).unwrap();
-
-    let result = Library::new(file_path.to_str().unwrap(), false);
+    let library = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
     temp_dir.close().unwrap();
-    assert!(matches!(result, Err(LibraryError::ReadFailed)));
+    assert!(library.is_empty());
 }
 
 #[test]
-fn test_container_new() {
+fn test_library_create() {
     let temp_dir = tempdir().unwrap();
-    let project_dir = temp_dir.path().join("project1");
-    fs::create_dir(project_dir).unwrap();
-
-    let result = Library::new(temp_dir.path().to_str().unwrap(), false);
-    assert!(result.is_ok());
+    let library = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
+    assert!(library.create("test_project").is_ok());
     temp_dir.close().unwrap();
-
-    let container = result.unwrap();
-    assert!(!container.is_empty());
-    assert!(container.contains("project1"));
 }
 
 #[test]
-fn test_container_new_display_hidden() {
+fn test_library_get() {
     let temp_dir = tempdir().unwrap();
-    let project_dir = temp_dir.path().join("project1");
-    let hidden_dir = temp_dir.path().join(".hidden_project");
+    let project_dir = temp_dir.path().join("test_project");
     fs::create_dir(project_dir).unwrap();
-    fs::create_dir(hidden_dir).unwrap();
-
-    let result = Library::new(temp_dir.path().to_str().unwrap(), false);
-    assert!(result.is_ok());
+    let library = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
     temp_dir.close().unwrap();
-
-    let container = result.unwrap();
-    assert!(!container.is_empty());
-    assert!(!container.contains(".hidden_project"));
-}
-
-#[test]
-fn test_container_contains() {
-    let temp_dir = tempdir().unwrap();
-    let project_dir = temp_dir.path().join("project1");
-    fs::create_dir(project_dir).unwrap();
-
-    let container = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
-    temp_dir.close().unwrap();
-    assert!(container.contains("project1"));
-    assert!(!container.contains("project2"));
-}
-
-#[test]
-fn test_container_get_vec() {
-    let temp_dir = tempdir().unwrap();
-    let project_dir = temp_dir.path().join("project1");
-    fs::create_dir(project_dir).unwrap();
-
-    let container = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
-    let projects = container.get_vec();
-    assert_eq!(projects.len(), 1);
-    assert_eq!(projects[0].get_name(), "project1");
-}
-
-#[test]
-fn test_container_get() {
-    let temp_dir = tempdir().unwrap();
-    let project_dir = temp_dir.path().join("project1");
-    fs::create_dir(project_dir).unwrap();
-
-    let container = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
-    temp_dir.close().unwrap();
-    let project = container.get("project1");
+    let project = library.get("test_project");
     assert!(project.is_some());
-    assert_eq!(project.unwrap().get_name(), "project1");
-
-    let project = container.get("project2");
-    assert!(project.is_none());
+    assert_eq!(project.unwrap().get_name(), "test_project");
 }
 
 #[test]
-fn test_container_is_empty() {
+fn test_library_contains() {
     let temp_dir = tempdir().unwrap();
-
-    let container = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
-    assert!(container.is_empty());
-
-    let project_dir = temp_dir.path().join("project1");
+    let project_dir = temp_dir.path().join("test_project");
     fs::create_dir(project_dir).unwrap();
-
-    let container = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
+    let library = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
     temp_dir.close().unwrap();
-    assert!(!container.is_empty());
+    assert!(library.contains("test_project"));
+}
+
+#[test]
+fn test_library_is_empty() {
+    let temp_dir = tempdir().unwrap();
+    let library = Library::new(temp_dir.path().to_str().unwrap(), false).unwrap();
+    assert!(library.is_empty());
+    temp_dir.close().unwrap();
 }
